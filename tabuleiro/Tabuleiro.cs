ea5d03ff4026380a862dfa0107eed4fca4421638
Lucas Ferreira <lucashf99@outlook.com.br>
2024-06-21 14:27:08 -0300

    Começando a colocar peças no tabuleiro

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -18,5 +18,11 @@ namespace tabuleiro
         {
             return Pecas[linhas, colunas];
         }
+
+        public void ColocarPeca(Peca p , Posicao pos)
+        {
+            Pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
+        }
     }
 }
