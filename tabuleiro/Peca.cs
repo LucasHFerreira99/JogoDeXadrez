7a5e55cb897db82bc4b30d43f8dacfbbedddaddb
Lucas Ferreira <lucashf99@outlook.com.br>
2024-06-21 12:18:03 -0300

    Começando a implementar o tabuleiro

---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace tabuleiro
+{
+    internal class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QtdMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+
+        public Peca(Posicao posicao, Tabuleiro tab , Cor cor)
+        {
+            Posicao = posicao;
+            Tab = tab;
+            Cor = cor;
+            QtdMovimentos = 0;
+        }
+
+    }
+}
