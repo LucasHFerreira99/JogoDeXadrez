e4936a6b035b98b7a8c05e63b045ad70bc11a033
Lucas Ferreira <lucashf99@outlook.com.br>
2024-06-25 17:40:55 -0300

    Jogada Especial Promocao

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -140,6 +140,20 @@ namespace xadrez
                 DesfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode se colocar em xeque!");
             }
+            Peca p = Tab.Peca(destino);
+
+            // #JogadaEspecial Promoção
+            if(p is Peao)
+            {
+                if((p.Cor == Cor.Branca && destino.Linha==0) || (p.Cor == Cor.Preta && destino.Linha == 7))
+                {
+                    p = Tab.RetirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(Tab, p.Cor);
+                    Tab.ColocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
 
             if (EstaEmXeque(Adversaria(JogadorAtual)))
             {
@@ -156,8 +170,6 @@ namespace xadrez
                 Turno++;
                 mudaJogador();
             }
-
-            Peca p = Tab.Peca(destino);
 
             // #JogadaEspecial En Passant
             if (p is Peao && destino.Linha == origem.Linha - 2 || destino.Linha == origem.Linha + 2)
